2244b2ea850944bb8d8c2c55b05869a81df6401c
Iqbal <iqbalchoudhury2@gmail.com>
2017-09-07 21:45:48 -0400

    code added resource helper and property file reader

--- src/test/java/com/facebook/cucumber_project/framework/stepdefination/LoginToFacebookStep.java
+++ src/test/java/com/facebook/cucumber_project/framework/stepdefination/LoginToFacebookStep.java
@@ -4,7 +4,9 @@
 package com.facebook.cucumber_project.framework.stepdefination;
 
 import cucumber.api.PendingException;
-import cucumber.api.java.en.*;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;;
 
 /**
  * @author choudhuryIqbal
