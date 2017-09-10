b75a2f132300829218f97939ab8ecf009a07076d
Iqbal <iqbalchoudhury2@gmail.com>
2017-09-10 03:54:41 -0400

    chromedriver added

--- src/test/java/com/facebook/cucumber_project/framework/runner/FacebookLoginFeatureRunner.java
+++ src/test/java/com/facebook/cucumber_project/framework/runner/FacebookLoginFeatureRunner.java
@@ -3,7 +3,11 @@ package com.facebook.cucumber_project.framework.runner;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-@CucumberOptions(features = { "classpath:featurefile/FacebookLogin.feature" })
+@CucumberOptions(features = { "classpath:featurefile/FacebookLogin.feature" },
+glue= {"classpath:com.facebook.cucumber_project.framework.stepdefination",
+		"classpath:com.facebook.cucumber_project.framework.helper"},
+plugin= {"pretty","json:target/FacebookLoginFeatureRunner.json"}
+		)
 public class FacebookLoginFeatureRunner extends AbstractTestNGCucumberTests {
 
 }
